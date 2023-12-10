e976459df2d1845628491a3029a4488c13106a40
Mukeswar Shah <mukeswar.github@gmail.com>
2023-12-10 10:52:39 +0545

    cryptoapp

---
+++ src/components/CoinCard.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import {Image, VStack,Heading,Text} from '@chakra-ui/react';
+
+   const CoinCard = ({id,name,img,symbol,price,currencySymbol="₹"})=>(
+    <Link to={`/coin/${id}`}>
+      <VStack w={"52"} shadow={"lg"} p={"8"} borderRadius={"lg"} transition={"all 0.3s"} m={"4"}
+      css={{
+        "&:hover":{
+          transform:"scale(1.1)",
+        }
+      }}
+      >
+      <Image 
+      src={img} 
+      width={"10"}
+       h={"10"}
+        objectFit = {"contain"}
+         alt={"Exchange"}/>
+         <Heading size={"md"} noOfLines={1}>{symbol}</Heading>
+         <Text noOfLines={1}>{name}</Text>
+         <Text noOfLines={1}>{price? `${currencySymbol}${price}`:"NA"}</Text>
+         </VStack>
+    </Link>
+  );
+
+export default CoinCard;
